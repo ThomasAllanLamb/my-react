46c3ecfa72da16cdab5651570a7e3667375a5029
Thomas Lamb <lambyte@lambyte.com>
2019-10-25 10:47:54 -0700

    Search works.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,94 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      search: 'ball'
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange (e) {
+    console.log("searchChange");
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        Enter a filter term to filter the list of goods and the catalog will update live.
+        <Search search={this.state.search} onSearchChange={this.onSearchChange} />
+        <Catalog search={this.state.search} products={this.props.products}/>
+      </div>
+    );
+  }
+}
+
+class Search extends React.Component {
+  render () {
+    return (
+      <div>
+        Filter: <input name="search" type="text" value={this.props.search} onChange={this.props.onSearchChange}></input>
+      </div>
+    );
+  }
+}
+
+class Catalog extends React.Component {
+  render () {
+    var rows = [];
+    var previousCategory;
+    var searchRegEx = new RegExp(this.props.search);
+    for (var i = 0; i <= this.props.products.length-1; i++)
+    {
+      let currentProduct = this.props.products[i];
+      
+      //ignore this product if it does not fit search parameters
+      if (searchRegEx.test(currentProduct.name) === false) continue;
+
+      //only display a header if the previous category was different from this one
+      if (previousCategory !== currentProduct.category)
+      {
+        //???: is there a better solution to this? we're creating virtual products, so they don't have IDs. However, there will be a maximum of one virtual product per real product, so we can use the negative versions of real product IDs. Zero doesn't work, though, so increment them by one to ensure that there is no product with ID 0.
+        rows.push(<CategoryRow key={-1*(currentProduct.category+1)} category={currentProduct.category} />);
+        previousCategory = currentProduct.category;
+      }
+
+      //add the product row
+      rows.push(<ProductRow key={currentProduct.id} product={currentProduct} />);
+    }
+    return (
+      <div>
+        Name
+        <ul>
+          {rows}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class CategoryRow extends React.Component {
+  //as this is a coding excercise, I am going to omit proper styling practices in favor of brevity
+  render () {
+    return (
+      <li><strong>{this.props.category}</strong></li>
+    );
+  }
+}
+
+class ProductRow extends React.Component {
+  render () {
+    return (
+      <li>{this.props.product.name}</li>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PRODUCTS = [
+  {id: 1, category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {id: 2, category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {id: 3, category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {id: 4, category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {id: 5, category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {id: 6, category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(<App products={PRODUCTS} />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
